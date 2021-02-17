7804d88ce902c40c66279431ee86bbf93c9deb9d
jose-marquez89 <jlmarquez89@gmail.com>
2021-02-16 19:55:20 -0600

    gets ticker data for specified currencies

---
+++ scripts/tickers.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+burl = "https://api.coingecko.com/api/v3"
+tickers = ["the-graph", "republic-protocol"]
+
+if __name__ == '__main__':
+    for currency in tickers:
+        endpoint = "/coins/{}/tickers".format(currency)
+        url = burl + endpoint
+        res = requests.get(url)
+        ticker_details = res.json()
+
+        fname = "{}.json".format(currency)
+
+        with open(fname, "w") as tdata:
+            json.dump(ticker_details, tdata)
